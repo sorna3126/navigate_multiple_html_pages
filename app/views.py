7412a56dc1730d497f7f8d8631e7df0ecd64280a
sorna3126 <sornak3105@gmail.com>
2023-11-23 13:10:12 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def harsha(request):
+    return render(request,'harsha.html')
+def varshu(request):
+    return render(request,'varshu.html')
+def harsha2(request):
+    return render(request,'harsha2.html')
